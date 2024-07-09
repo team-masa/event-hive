8ea193d0e9872e1d3bec55a82382b8bbe8672d25
Mabel Aikins Acquah <acquahmabel60@gmail.com>
2024-07-09 11:20:01 +0000

    Changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import RootLayout from "./layouts/rootLayout";
+import Landing from './pages/landing-page';
+import SignIn from './pages/sign-in';
+import SignUp from './pages/sign-up';
+import Event from './pages/event';
+import College from './pages/college';
+import CreateEvent from './pages/create-event';
+import NotFound from './pages/404';
+import SignInLayout from './layouts/signInLayout';
+import SignLayout from './layouts/signUpLayout';
+import CreateEventLayout from './layouts/createEventLayout';
 
 
 function App() {
@@ -10,10 +20,89 @@ function App() {
     {
       path: "/",
       element: <RootLayout />,
-      children: []
+      children: [
+        {
+          index: true,
+          path: "/",
+          element: <Landing />
+        },
 
         
+
+        {
+          path:"/event",
+          element:<Event/>
+        },
+
+        {
+          path:"/college",
+          element:<College/>
+        },
+
+        
+
+        {
+          path:"/404",
+          element:<NotFound/>
+        }
+      ]
+
+
     },
+
+
+    {
+      path: "/",
+      element: <SignInLayout />,
+      children: [
+        
+          
+        {
+          index: true,
+          path: "/sign-in",
+          element:<SignIn/>
+        },
+
+      ]
+
+
+    },
+
+    {
+      path: "/",
+      element: <SignLayout />,
+      children: [
+        
+          
+        {
+          index: true,
+          path: "/sign-UP",
+          element:<SignUp/>
+        },
+
+      ]
+
+
+    },
+
+
+    {
+      path: "/",
+      element: <CreateEventLayout />,
+      children: [
+        
+          
+        {
+          index: true,
+          path: "/create-event",
+          element:<CreateEvent/>
+        },
+
+      ]
+
+
+    },
+
 
   ]);
 
---
+++ src/layouts/signUpLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignUp from '../pages/sign-up'
+
+const SignLayout = () => {
+  return (
+    <div>
+        <SignUp/>
+    </div>
+  )
+}
+
+export default SignLayout
